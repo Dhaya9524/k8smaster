4002c8a8208e50a948aaaeac8331034426240869
Dhaya9524 <dhaya9524@gmail.com>
2025-07-28 12:13:05 +0000

    post script

---
+++ modules/vm/post.tf
@@ -0,0 +1,17 @@
+resource "null_resource" "remote_command" {
+    depends_on = [ proxmox_vm_qemu.this ]
+    provisioner "remote-exec" {
+        inline = [
+            "sudo sed -i 's/^#\\?PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf",
+            "sudo systemctl restart sshd"
+        ]
+        connection {
+            type        = "ssh"
+            host        = proxmox_vm_qemu.this.ip_address
+            user        = var.ciuser
+            password    = var.cipassword
+            timeout     = "5m"
+            agent       = false
+        }
+    }
+}
